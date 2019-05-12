5d126b1b4c8aa48b4cf1b0411bdf1c1d4b83cfa3
whatFruit <matthew.s.atteberry@gmail.com>
2019-05-11 21:30:11 -0700

    Added README, implemented boiler plate loop for pygame/pymunk

---
+++ README.md
@@ -0,0 +1,17 @@
+#Mountain Car RL
+#####Author: Matthew Atteberry
+
+###Overview:
+The Mountain Car problem is a classic machine learning task. The set-up is a 'car' (in this excerise, a simple ball), that can accelerate left, right, or stay in neutral (its **action**). This car starts at the bottom of a valley that it can not simply accelerate out of, but must build momentum by going up left and right slopes in order to climb the 'mountain'. Machine learning is used to formulate a policy based on the car's position and velocity (its **state**) that directs the car out of the valley.
+
+####Dependencies
+- [Pymunk](https://github.com/viblo/pymunk)
+- [Pygame](https://github.com/pygame/pygame)
+
+####Status: WIP
+- [x] Setup Basic Pygame/Pymunk scene and loop
+- [ ] Setup Mountain and Car sprites/physics objects
+- [ ] Provide Access to cars state and actions as an RL agent
+- [ ] Design and implement Value Approximation RL methods
+- [ ] Hook up RL training method with car agent
+- [ ] Design and implement a visualization of the car agent's policy 
--- car.py
+++ car.py
@@ -1,7 +1,48 @@
-#GUI and Physics depiction of a car(ball) in a mountain valley
+# GUI and Physics depiction of a car(ball) in a mountain valley
+# Author: Matthew Atteberry
+
+import pygame
+from pygame.locals import * # grant easy access to pygame constants
+import pymunk
+
+SCENE_WIDTH = 600
+SCENE_HEIGHT = 600
+PYMUNK_GRAVITY = -981.0
+DT = 1.0/60.0 # define physics time step
+DT_MS = DT * 1000 # time step in milliseconds
 
 def main():
-    print("")
+
+    ### Initialize pygame instance
+
+    pygame.init()
+    running = True #flag to signal end of scene
+
+    ### setup pygame scene
+
+    scene = pygame.display.set_mode((SCENE_WIDTH, SCENE_HEIGHT))
+    clock = pygame.time.Clock()
+
+    ### PyMunk Physics
+
+    physSpace = pymunk.Space() # this is pymunk physics simulation container
+    physSpace.gravity = 0.0, PYMUNK_GRAVITY
+
+    ### Core scene loop
+
+    while running:
+        # handle pygame events
+        for event in pygame.event.get():
+            if event.type == QUIT or (event.type == KEYDOWN and event.key == K_ESCAPE):
+                running = False
+
+        # physics upkeep
+        physSpace.step(DT)
+
+        # scene update
+        clock.tick(DT_MS)
+
+    print("End of Scene")
 
 if __name__ == '__main__':
     main()
